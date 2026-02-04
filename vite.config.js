59595bcbb46a26d92c154dadf65848e05ae3381e
Ricardo Ferreira <ricardoferreirades@gmail.com>
2026-02-04 23:17:24 +0100

    chore: add project configuration files

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: false, // If port 3000 is in use, try next available
+  },
+})
+
